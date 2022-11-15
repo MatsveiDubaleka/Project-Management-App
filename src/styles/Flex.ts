9d64b1e44a1164b6af183b5661c53d8debcefbb5
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-16 02:24:27 +0300

    refactor: add global style Flex Wed, 16 Nov 2022 02:24:27 +0300

---
+++ src/styles/Flex.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export interface FlexProps {
+  direction?: 'row' | 'column';
+  justify?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around';
+  align?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
+  wrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
+  gap?: string;
+}
+
+export const Flex = styled.div<FlexProps>`
+  display: flex;
+  flex-direction: ${(props) => props.direction || 'row'};
+  align-items: ${(props) => props.align || 'center'};
+  justify-content: ${(props) => props.justify || 'flex-start'};
+  gap: ${(props) => props.gap || 0};
+`;
