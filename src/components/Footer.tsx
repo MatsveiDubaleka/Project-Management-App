d06fa607360a05cc6f80ac07d29c85604f30575f
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-18 13:08:09 +0300

    fix: fix footer Fri, 18 Nov 2022 13:08:09 +0300

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,21 @@ import Logo from 'assets/svg/course-logo.svg';
 import styled from 'styled-components';
 
 const StyledFlex = styled(Flex)`
-  position: fixed;
+  max-width: 1280px;
+  margin: 0 auto;
   width: 100%;
   height: 100px;
-  padding: 0 5vw;
-  left: 0;
-  bottom: 0;
   text-align: center;
   background-color: #282c34;
   color: #fff;
+`;
+
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: #282c34;
 `;
 
 const StyledLink = styled.a`
@@ -22,25 +28,27 @@ const StyledLink = styled.a`
 
 const Footer = () => {
   return (
-    <StyledFlex justify="space-between">
-      <div className="creation-year">2022 &#169;</div>
-      <Flex gap="50px" className="authors">
-        <StyledLink href="https://github.com/aibolit666" target="_blank" rel="noreferrer">
-          Pavel Priladyshev
-        </StyledLink>
-        <StyledLink href="https://github.com/AlexandrKlychnikov" target="_blank" rel="noreferrer">
-          Alexandr Klychnikov
-        </StyledLink>
-        <StyledLink href="https://github.com/MatsveiDubaleka" target="_blank" rel="noreferrer">
-          Matsvei Dubaleka
-        </StyledLink>
-      </Flex>
-      <div className="course-logo">
-        <a href="https://rs.school" target="_blank" rel="noreferrer">
-          <img src={Logo} alt="RSSCHOOL Course" width="150px" />
-        </a>
-      </div>
-    </StyledFlex>
+    <StyledFooter>
+      <StyledFlex justify="space-between">
+        <div className="creation-year">2022 &#169;</div>
+        <Flex gap="50px" className="authors">
+          <StyledLink href="https://github.com/aibolit666" target="_blank" rel="noreferrer">
+            Pavel Priladyshev
+          </StyledLink>
+          <StyledLink href="https://github.com/AlexandrKlychnikov" target="_blank" rel="noreferrer">
+            Alexandr Klychnikov
+          </StyledLink>
+          <StyledLink href="https://github.com/MatsveiDubaleka" target="_blank" rel="noreferrer">
+            Matsvei Dubaleka
+          </StyledLink>
+        </Flex>
+        <div className="course-logo">
+          <a href="https://rs.school" target="_blank" rel="noreferrer">
+            <img src={Logo} alt="RSSCHOOL Course" width="150px" />
+          </a>
+        </div>
+      </StyledFlex>
+    </StyledFooter>
   );
 };
 
