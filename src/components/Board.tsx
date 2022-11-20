e2011e4c10b57bd4ae9711334ff66f2764400af8
Pavel <pavel.priladyshev@gmail.com>
2022-11-20 20:36:15 +0300

    feat: add check token for null and undefined in chechTokenValidation

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -10,8 +10,6 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { useNavigate } from 'react-router-dom';
-import { Endpoint } from 'constants/endpoints';
 
 const Board = styled.div`
    {
