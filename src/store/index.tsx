582405198db3bfdb85f412b77204992d9aacdcfa
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-26 21:51:48 +0300

    feat: add loadingSlice reducer Sat, 26 Nov 2022 21:51:47 +0300

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import columnReducer from './slices/columnSlice';
 import taskReducer from './slices/taskSlice';
+import loadingSlice from './slices/loadingSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     columns: columnReducer.reducer,
     tasks: taskReducer.reducer,
+    loading: loadingSlice.reducer,
   },
 });
 
